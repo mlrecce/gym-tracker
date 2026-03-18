bd63358ac91778dcbcfe28e8a1cf87111a7c860b
mlrecce <mlrecce@gmail.com>
2026-03-18 11:55:51 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+var CACHE_NAME = 'gym-tracker-v1';
+var URLS_TO_CACHE = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(URLS_TO_CACHE); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(names) {
+    return Promise.all(names.filter(function(n) { return n !== CACHE_NAME; }).map(function(n) { return caches.delete(n); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
+});
